eb087406cc23cdfbdf0cdfa6f4a18727ef708597
Kenichi Tachibana <deadzone@be.to>
2013-05-09 00:03:19 +0900

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kumiter::Application.config.secret_key_base = 'e58a5c19c6d087b815b6fb4bc68dc9a9ae51c3c1b1cf051387b8dbed20d9daf3cc6f5fa6df767bd08d232aac5a04bd00993f80ee49841af0b80a4126bed9985f'
